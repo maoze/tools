db7e14de9205f689d6caf8cb64990e46615c3257
maoze <maoze8505@163.com>
2018-06-11 21:47:34 +0800

    Refine code for already exist repo

--- vim-go/InstallVimGoBinaries.sh
+++ vim-go/InstallVimGoBinaries.sh
@@ -1,32 +1,73 @@
 #!/bin/bash
 SRC_PATH=`go env GOPATH`/src
-git clone https://github.com/nsf/gocode  ${SRC_PATH}/github.com/nsf/gocode
-git clone https://github.com/alecthomas/gometalinter.git ${SRC_PATH}/github.com/alecthomas/gometalinter
-git clone https://github.com/golang/tools.git ${SRC_PATH}/golang.org/x/tools
-git clone https://github.com/golang/lint.git ${SRC_PATH}/github.com/golang/lint
-git clone https://github.com/rogpeppe/godef.git ${SRC_PATH}/github.com/rogpeppe/godef
-git clone https://github.com/kisielk/errcheck.git ${SRC_PATH}/github.com/kisielk/errcheck
-git clone https://github.com/jstemmer/gotags.git ${SRC_PATH}/github.com/jstemmer/gotags
-git clone https://github.com/klauspost/asmfmt.git ${SRC_PATH}/github.com/klauspost/asmfmt
-git clone https://github.com/fatih/motion.git ${SRC_PATH}/github.com/fatih/motion
-git clone https://github.com/fatih/gomodifytags.git ${SRC_PATH}/github.com/fatih/gomodifytags
-git clone https://github.com/zmb3/gogetdoc.git ${SRC_PATH}/github.com/zmb3/gogetdoc
-git clone https://github.com/josharian/impl.git ${SRC_PATH}/github.com/josharian/impl
-git clone https://github.com/dominikh/go-tools.git ${SRC_PATH}/github.com/dominikh/go-tools
-git clone https://github.com/davidrjenni/reftools.git ${SRC_PATH}/github.com/davidrjenni/reftools
-go get github.com/nsf/gocode
-go get github.com/alecthomas/gometalinter
-go get golang.org/x/tools/cmd/goimports
-go get golang.org/x/tools/cmd/guru
-go get golang.org/x/tools/cmd/gorename
-go get github.com/golang/lint/golint
-go get github.com/rogpeppe/godef
-go get github.com/kisielk/errcheck
-go get github.com/jstemmer/gotags
-go get github.com/klauspost/asmfmt/cmd/asmfmt
-go get github.com/fatih/motion
-go get github.com/fatih/gomodifytags
-go get github.com/zmb3/gogetdoc
-go get github.com/josharian/impl
-go get github.com/dominikh/go-tools/cmd/keyify
-go get github.com/davidrjenni/reftools/cmd/fillstruct
+COMPONENTS_SRC=("github.com/nsf/gocode" \
+	"github.com/alecthomas/gometalinter" \
+	"github.com/golang/tools" \
+	"github.com/golang/lint" \
+	"github.com/rogpeppe/godef" \
+	"github.com/kisielk/errcheck" \
+	"github.com/jstemmer/gotags" \
+	"github.com/klauspost/asmfmt" \
+	"github.com/fatih/motion" \
+	"github.com/fatih/gomodifytags" \
+	"github.com/zmb3/gogetdoc" \
+	"github.com/josharian/impl" \
+	"github.com/dominikh/go-tools" \
+	"github.com/davidrjenni/reftools")
+COMPONENTS_DST=("github.com/nsf/gocode" \
+	"github.com/alecthomas/gometalinter" \
+	"golang.org/x/tools" \
+	"golang.org/x/lint" \
+	"github.com/rogpeppe/godef" \
+	"github.com/kisielk/errcheck" \
+	"github.com/jstemmer/gotags" \
+	"github.com/klauspost/asmfmt" \
+	"github.com/fatih/motion" \
+	"github.com/fatih/gomodifytags" \
+	"github.com/zmb3/gogetdoc" \
+	"github.com/josharian/impl" \
+	"github.com/dominikh/go-tools" \
+	"github.com/davidrjenni/reftools")
+COMPONENTS_BUILD=("github.com/nsf/gocode" \
+	"github.com/alecthomas/gometalinter" \
+	"golang.org/x/tools/cmd/goimports" \
+	"golang.org/x/tools/cmd/guru" \
+	"golang.org/x/tools/cmd/gorename" \
+	"golang.org/x/lint/golint" \
+	"github.com/rogpeppe/godef" \
+	"github.com/kisielk/errcheck" \
+	"github.com/jstemmer/gotags" \
+	"github.com/klauspost/asmfmt/cmd/asmfmt" \
+	"github.com/fatih/motion" \
+	"github.com/fatih/gomodifytags" \
+	"github.com/zmb3/gogetdoc" \
+	"github.com/josharian/impl" \
+	"github.com/dominikh/go-tools/cmd/keyify" \
+	"github.com/davidrjenni/reftools/cmd/fillstruct")
+
+for ((i=0; i<${#COMPONENTS_SRC[*]};i++))
+do
+	git clone https://${COMPONENTS_SRC[$i]}.git ${SRC_PATH}/${COMPONENTS_DST[$i]} >/dev/null 2>&1
+	if [ $? -eq 128 ]
+	then
+		echo "You get ${SRC_PATH}/${COMPONENTS_DST[$i]} before. Now update it"
+		pushd ${SRC_PATH}/${COMPONENTS_DST[$i]} >/dev/null
+		git pull
+		popd > /dev/null
+	elif [ $? -eq 0 ]
+	then
+		echo "Download ${SRC_PATH}/${COMPONENTS_DST[$i]} Successfully"
+	else
+		echo "Download ${SRC_PATH}/${COMPONENTS_DST[$i]} Failed"
+	fi
+done
+for ((i=0; i<${#COMPONENTS_BUILD[*]};i++))
+do
+	go get ${COMPONENTS_BUILD[$i]}
+	if [ $? -eq 0 ]
+	then
+		echo "${COMPONENTS_BUILD[$i]} build Successfully"
+	else
+		echo "${COMPONENTS_BUILD[$i]} build Failed"
+	fi
+done
